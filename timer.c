1724c3456f0487537dc5b6684f52e012a013758d
Axait-th <axaitth@gmail.com>
2025-07-25 08:39:05 +0500

    Initial Commit

---
+++ timer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("                      d8,                                \n");
+    printf("               d8P   `8P                                 \n");
+    printf("            d888888P                                     \n");
+    printf("  d8888b      ?88'    88b  88bd8b,d88b  d8888b  88bd88b  \n");
+    printf(" d8P' `P      88P     88P  88P'`?8P'?8bd8b_,dP  88P'  `  \n");
+    printf(" 88b          88b    d88  d88  d88  88P88b     d88       \n");
+    printf(" `?888P'      `?8b  d88' d88' d88'  88b`?888P'd88'       \n");
+    printf("                                                         \n");
+    printf("                                           -By Axait-th  \n");
+    printf("                                                         \n");
+                                                      
+    return 0;
+}
