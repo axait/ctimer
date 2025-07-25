af76d822f84606277908d52600631e4fce34ba8d
Axait-th <axaitth@gmail.com>
2025-07-25 12:03:53 +0500

    Add better sleep in timerUpdater. & add clearing Console Before Starting. &  add colorHelp  func that contains all colors related comments.  &  stylized timer Number output.

--- timer.c
+++ timer.c
@@ -1,47 +1,73 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-#include <stdlib.h>
+#include <unistd.h> // for sleep, ... funcs
+#include <signal.h> // for Handling Ctrl+C
+#include <stdlib.h> // for exit() func.
 
-//   _________________________________
-//  | Escape Code | Action           |
-//  | ----------- | ---------------- |
-//  | `\033[A`    | Up 1 line        |
-//  | `\033[2A`   | Up 2 lines       |
-//  | `\033[B`    | Down 1 line      |
-//  | `\033[C`    | Right 1 column   |
-//  | `\033[D`    | Left 1 column    |
-//  | `\033[n;Cm` | Set color        |
-//  | `\033[H`    | Move to top-left |
-//  ---------------------------------
 
+void colorHelp(){
+    // Don't look inside it. it is just for Color Help. It is used No where.
+
+    //   _________________________________
+    //  | Escape Code | Action           |
+    //  | ----------- | ---------------- |
+    //  | `\033[A`    | Up 1 line        |
+    //  | `\033[2A`   | Up 2 lines       |
+    //  | `\033[B`    | Down 1 line      |
+    //  | `\033[C`    | Right 1 column   |
+    //  | `\033[D`    | Left 1 column    |
+    //  | `\033[n;Cm` | Set color        |
+    //  | `\033[H`    | Move to top-left |
+    //  ---------------------------------
+
+
+    //  --------------------------------------------
+    // | Color   | Code | Example                  |
+    // | ------- | ---- | ------------------------ |
+    // | Black   | `30` | `\033[30mBlack\033[0m`   |
+    // | Red     | `31` | `\033[31mRed\033[0m`     |
+    // | Green   | `32` | `\033[32mGreen\033[0m`   |
+    // | Yellow  | `33` | `\033[33mYellow\033[0m`  |
+    // | Blue    | `34` | `\033[34mBlue\033[0m`    |
+    // | Magenta | `35` | `\033[35mMagenta\033[0m` |
+    // | Cyan    | `36` | `\033[36mCyan\033[0m`    |
+    // | White   | `37` | `\033[37mWhite\033[0m`   |
+    //  --------------------------------------------
+
+    //  ----------------------------------------------------------------
+    // | Style          | Code    | Example                            |
+    // | -------------- | ------- | ---------------------------------- |
+    // | **Bold**       | `1`     | `\033[1;31mBold Red\033[0m`        |
+    // | **Underline**  | `4`     | `\033[4;34mUnderlined Blue\033[0m` |
+    // | **Background** | `41–47` | `\033[30;47mBlack on White\033[0m` |
+    //  ----------------------------------------------------------------
+
+}
 
 int banner(){
     // Bright Green Color
     // 1 for bold, 31 for red
     // 4 for underline, 32 for green
-    printf("\033[1;32m                      d8,                               \033[0m \n");
-    printf("\033[1;32m               d8P   `8P                                \033[0m \n");
-    printf("\033[1;32m            d888888P                                    \033[0m \n");
-    printf("\033[1;32m  d8888b      ?88'    88b  88bd8b,d88b  d8888b  88bd88b \033[0m \n");
-    printf("\033[1;32m d8P' `P      88P     88P  88P'`?8P'?8bd8b_,dP  88P'  ` \033[0m \n");
-    printf("\033[1;32m 88b          88b    d88  d88  d88  88P88b     d88      \033[0m \n");
-    printf("\033[1;32m `?888P'      `?8b  d88' d88' d88'  88b`?888P'd88'      \033[0m \n");
-    printf("\033[1;32m                                                        \033[0m \n");
+    printf("\033[1;33m                      d8,                               \033[0m \n");
+    printf("\033[1;35m               d8P   `8P                                \033[0m \n");
+    printf("\033[1;33m            d888888P                                    \033[0m \n");
+    printf("\033[1;34m  d8888b      ?88'    88b  88bd8b,d88b  d8888b  88bd88b \033[0m \n");
+    printf("\033[1;35m d8P' `P      88P     88P  88P'`?8P'?8bd8b_,dP  88P'  ` \033[0m \n");
+    printf("\033[1;36m 88b          88b    d88  d88  d88  88P88b     d88      \033[0m \n");
+    printf("\033[1;33m `?888P'      `?8b  d88' d88' d88'  88b`?888P'd88'      \033[0m \n");
+    printf("                                                        \033[0m \n");
     printf("                                            -By \033[4;34mAxait-th\033[0m \n"); // Red Color, for Creator.
     printf("                                                         \n");
     sleep(1);  // for UX
 }
 
-
 int timerShower(int Years ,int Months ,int Days ,int Hours , int Minutes ,int Seconds ){
 
-    printf(" &&   \033[1;31m%d\033[0m  "   , Years    );
-    printf(" &&   \033[1;31m%d\033[0m   "  , Months   );
-    printf(" &&  \033[1;31m%d\033[0m  "    , Days     );
-    printf(" &&   \033[1;31m%d\033[0m  "   , Hours    );
-    printf(" &&    \033[1;31m%d\033[0m   " , Minutes  );
-    printf(" &&    \033[1;31m%02d\033[0m  "  , Seconds  );
+    printf(" &&   \033[1;3;36m%d\033[0m  "   , Years    );
+    printf(" &&   \033[1;3;36m%d\033[0m   "  , Months   );
+    printf(" &&  \033[1;3;36m%d\033[0m  "    , Days     );
+    printf(" &&   \033[1;3;36m%d\033[0m  "   , Hours    );
+    printf(" &&    \033[1;3;36m%d\033[0m   " , Minutes  );
+    printf(" &&    \033[1;3;36m%02d\033[0m  "  , Seconds  );
     printf(" && \n");
     printf(" ============================================================ \n");
     printf("\n  \n"); // Blank Line to easily remove ^C from stdout. Like if ^C does not shown in stdout in other OS.
@@ -61,7 +87,6 @@ int timerLayout( ){
     return 0;
 }
 
-
 int timerUpdater( ){
 
     int Years   = 0 ; 
@@ -73,7 +98,10 @@ int timerUpdater( ){
 
     // Sending Loop to Code to Start Timer.
     while (1){
-        sleep(1);
+        // usleep(1000) = 1 millisecond
+        // 1000 millisecond = 1 second
+        // for readable conversion to milliSeconds.
+        usleep(999*1000);
 
         // To Update Seconds Just.
         Seconds++;
@@ -122,23 +150,32 @@ int timerUpdater( ){
     return 0;
 }
 
+void clearConsole() {
+    #ifdef _WIN32
+        system("cls");     // Windows
+    #else
+        system("clear");   // Unix/Linux/macOS
+    #endif
+}
 
-// Handler for SIGINT (Ctrl+C)
 void handleSigint(int sig) {
+    // Handler for SIGINT (Ctrl+C)
+
     // To Remove ^C from stdout
     printf("\033[2D");
     // underlining Link with Yellow color (Better visibility).
     printf("🛑 Why not to vist \033[4;33mhttps://github.com/axait/\033[0m\n");
-    printf(" Caught Ctrl+C ! Exiting ...\n");
-    sleep(1);
+    printf(" Caught Ctrl+C ! Exiting (2s) ...\n");
+    sleep(2);
     // You can do cleanup here
     exit(0);  // Exit gracefully by returning 0 to OS,
 }
 
 int main(){
     
-    // banner();
-    timerLayout();   // to set default value of layout at beginning
+    clearConsole();
+    banner();
+    timerLayout();
 
     signal(SIGINT, handleSigint);
     timerUpdater();  
